eb4e1e94c67857345fcd6c14fd13684cd7e2d47a
Kimdoyeop <dpdp39000@gmail.com>
2022-12-05 07:40:39 +0900

    Study Spring :: 회원관리예제

---
+++ src/main/java/hello/helloSpring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.helloSpring.controller;
+
+public class MemberForm {
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    private String Name;
+}
